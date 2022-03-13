eb4a342fd95926acecf514bcf7ad6ead2160ab84
Mehmet Emin Araz <mehmeteminaraz@gmail.com>
2022-03-13 10:36:30 +0300

    SeleniumGrid_Jenkins

---
+++ src/main/java/GridSetupTest/SeleniumGrid/App.java
@@ -0,0 +1,13 @@
+package GridSetupTest.SeleniumGrid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/GridSetupTest/SeleniumGrid/AppTest.java
@@ -0,0 +1,37 @@
+package GridSetupTest.SeleniumGrid;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void Test1()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void Test2()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void Test3()
+    {
+        assertTrue( false );
+    } 
+    @Test
+    public void Test4()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void Test5()
+    {
+        assertTrue( true );
+    }
+}
